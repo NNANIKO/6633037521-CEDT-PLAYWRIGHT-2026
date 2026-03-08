f4f735d2e6b92f41a4b261e9d8365d16d37916b6
NNANIKO <naniko.study@gmail.com>
2026-03-08 18:52:20 +0700

    Assignment 3

--- page-objects/loginPage.js
+++ page-objects/loginPage.js
@@ -1,15 +1,18 @@
 export class LoginPage {
 	constructor(page) {
 		this.page = page
+		this.username = process.env.APP_USER || ''
+		this.password = process.env.PASSWORD || ''
+		this.url = process.env.URL || ''
 	}
 
-	async login({ username, password }) {
-		await this.page.goto('https://katalon-demo-cura.herokuapp.com/')
+	async login() {
+		await this.page.goto(this.url)
 		await this.page.getByRole('link', { name: 'Make Appointment' }).click()
 		await this.page.getByLabel('Username').click()
-		await this.page.getByLabel('Username').fill(username)
+		await this.page.getByLabel('Username').fill(this.username)
 		await this.page.getByLabel('Password').click()
-		await this.page.getByLabel('Password').fill(password)
+		await this.page.getByLabel('Password').fill(this.password)
 		await this.page.getByRole('button', { name: 'Login' }).click()
 	}
 }
